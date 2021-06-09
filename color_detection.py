c4d104ce13c7ec95784551aead2f688ead01abd8
Sachin Lodhi <sachinlodhi8614@gmail.com>
2021-06-09 19:32:56 +0530

    Update color_detection.py

--- color_detection.py
+++ color_detection.py
@@ -1,10 +1,22 @@
+
+########### COLOR IDENTIFICATION IN THE IMAGE #########################
+
+#########################################################################
+#   Author : Sachin Lodhi                                               #
+#   Track  : Computer Vision and Internet of Things                     #
+#   Batch  : June 2021                                                  #
+#   Assignment 2 : COLOR IDENTIFICATION IN THE IMAGE
+#   Graduate Rotational Internship Program,                             #
+#   The Spark Foundation, June 2021                                     #
+#                                                                       #
+#########################################################################
+
+
 import cv2 as cv
 import colorPredictor as cp
 import argparse
 # Argument Parsing
-
 parser = argparse.ArgumentParser()
-
 parser.add_argument("-i", "--imagePath", required=True,
                     help = "Full path of the image file that is to be processed for color detection")
 args = vars(parser.parse_args())
@@ -15,6 +27,8 @@ args = vars(parser.parse_args())
 clicked = False
 r = g = b = xpos = ypos = 0
 
+
+# initilaization with class
 class colorDetection():
     def __init__(self, img):
         self.img = img
@@ -47,28 +61,23 @@ def detect(img):
 
         cv.imshow("image", img)
         if (clicked):
-
-            # cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle
+            # Rectangular Boundary
             cv.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
 
-            # Creating text string to display( Color name and RGB values )
-            # text = cDetector.getColorName(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+            # Color Information with Possible Color Name and its RGB value
             text = 'Best Possible Color : ' + cp.convert_rgb_to_names((r,g,b)) +  ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
 
-            # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
+            # Writing text on image
             cv.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv.LINE_AA)
 
-            # For very light colours we will display text in black colour
+            # for light colors the information would be in dark color
             if (r + g + b >= 600):
                 cv.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv.LINE_AA)
 
             clicked = False
 
-
-        # Break the loop when user hits 'esc' key
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
-
     cv.destroyAllWindows()
 
 if __name__ == '__main__':
